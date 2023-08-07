c8c6b54c9337385d9cd25d89cd951063df6c2f16
Nitsugama <pinhatapinha@gmail.com>
2023-08-07 17:53:18 -0300

    server initialization

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const epxress = require("express");
+
+const api = express();
+
+api.get('/', () => {
+    resizeBy.json{(message: "Bem vindoa a nossa API")}
+});
+
+api.listen(3000,  () =>{
+    res.json{(message: )}
+})
